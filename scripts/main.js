d6ac98af55a12bfd7a2b8798a33a37b3409f8d64
adnan <adnan97m@gmail.com>
2021-02-20 18:09:51 +0100

    pause menu

--- scripts/main.js
+++ scripts/main.js
@@ -37,7 +37,6 @@ function OnBeforeProjectStart(runtime)
 
 	runtime.addEventListener("tick", () => Tick());
 	
-	runtime.addEventListener("keydown", e => OnKeyDown(e));
 	runtime.addEventListener("pointerdown", e => PointerManager.addPointer(e.pointerId, e.clientX, e.clientY));
 	runtime.addEventListener("pointermove", e => PointerManager.movePointer(e.pointerId, e.clientX, e.clientY));
 	runtime.addEventListener("pointerup", e => PointerManager.removePointer(e.pointerId));
@@ -61,24 +60,4 @@ function Tick()
 	{
 		LayoutMainManager.tick();
 	}
-}
-
-function OnKeyDown(e)
-{
-	const runtime = globalThis.runtime;
-	
-	if (e.key == 'Escape')
-	{
-		runtime.globalVars.layoutTransitionScheduled = false;
-		runtime.goToLayout("menu");
-		
-		return;
-		//pause menu stuff or getting back from input section
-	}
-
-// 	const inputTypes = [runtime.globalVars.input_0_type, runtime.globalVars.input_1_type];
-
-// 	if (false == inputTypes.includes('KEYBOARD_AD') && false == inputTypes.includes('KEYBOARD_ARROW')) {
-// 		return;
-// 	}
 }
